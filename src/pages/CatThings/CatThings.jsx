c528298d478619b67ddaed8667dddf84a8117cda
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-10-12 15:37:50 -0400

    add carlas things

---
+++ src/pages/CatThings/CatThings.jsx
@@ -0,0 +1,16 @@
+import ThingCard from '../../components/ThingCard/ThingCard'
+import { Link } from 'react-router-dom'
+
+const CatThings = (props) => {
+  return (
+    <>
+      <h1>Carla's Things</h1>
+      <Link to="/">Home</Link>
+
+      {props.things.map((thing, idx) => 
+        <ThingCard key={idx} thing={thing}/>
+      )}
+    </>
+  )
+}
+export default CatThings
